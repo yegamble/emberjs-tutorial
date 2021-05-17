52d46adc6897bdd2d7e1d57cbbf177ff25fe579a
Yosef Gamble <yegamble@gmail.com>
2021-05-17 22:39:22 +1200

    Initial commit

---
+++ app/controllers/cart.js
@@ -0,0 +1,20 @@
+import Controller from '@ember/controller';
+
+export default class CartController extends Controller {
+  subtotal = 0;
+
+  get subtotal() {
+    return this.model.reduce((acc, item) => {
+      return acc + item.price;
+    });
+  }
+
+  get tax() {
+    return 0.09 * this.subtotal;
+  }
+
+  get total() {
+    return this.subtotal + this.tax;
+  }
+
+}
