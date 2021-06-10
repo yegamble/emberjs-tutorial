5a2886be9f422b5a0eb53506b9af36e9f0952e59
Yosef Gamble <yosef@realestate.co.nz>
2021-06-10 16:07:49 +1200

    update to tutorial 7

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,7 @@
 import Route from '@ember/routing/route';
+import { products } from '../data/products'
 
 export default class IndexRoute extends Route {
+
+    model(){return products}
 }
--- app/routes/item.js
+++ app/routes/item.js
@@ -1,8 +1,18 @@
 import Route from '@ember/routing/route';
+import { products } from '../data/products';
 
 export default class ItemRoute extends Route {
-  model(params) {
-    const { item_id } = params;
-    return item_id;
-  }
+    model(params){
+    const {
+      item_id
+    } = params;
+    const product = products.find(({id})  => id === item_id);
+    console.log(product);
+    return product;
+    }
+
+    setupController(controller, model){
+      super.setupController(controller,model);
+      controller.color = model.colors[0].color;
+    }
 }
